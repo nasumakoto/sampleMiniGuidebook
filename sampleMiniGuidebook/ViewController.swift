3a043d735dc805b7334ded010a9f8d0c00556b95
那須真 <nasumakoto@nasumakotonoMacBook-Pro.local>
2017-06-19 13:42:39 +0800

    Initial Commit

---
+++ sampleMiniGuidebook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleMiniGuidebook
+//
+//  Created by 那須真 on 2017/06/19.
+//  Copyright © 2017年 Makoto Nasu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
