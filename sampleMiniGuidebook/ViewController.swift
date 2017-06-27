41e4c7ef2c75ea39744c09dce38599c9bbbd3453
那須真 <flamingo.nasu@gmail.com>
2017-06-27 14:30:27 +0800

    初めてのコミット

--- sampleMiniGuidebook/ViewController.swift
+++ sampleMiniGuidebook/ViewController.swift
@@ -8,11 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+
+    // プロパティリストから読み込んだdataを格納する配列
+    // [string] -> 中身がstring型のデータが入っている配列
+    var placeList:[String] = []
+    
+    var selectName = ""
+    
+    // TODO:2枚目の画面を作成し、タップされたエリア名を2枚目の画面で表示する（ラベル仕様）
+    // エリア名をそのまま2枚目の画面に渡す（数字ではなく文字）
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // プロパティリストファイルパス取得
+        let filePath = Bundle.main.path(forResource: "placelist", ofType: "plist")
+        
+        // プロパティリストからdataを取得（ディクショナリー型）
+        let dic = NSDictionary(contentsOfFile: filePath!)
+        
+        // Tableviewで扱いやすい配列（エリアの入っている配列）を作成
+        // 高速列挙で（for文）で値を取得
+        for (key,data) in dic! {
+//            print("KEY=\(key)")
+//            print("DATA-\(data)")
+            placeList.append(key as! String)
+        }
+        
+        // printで配列の中身を確認（念のため）
+        print(placeList)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeList.count
+    }
+    
+    // ③リクエストに表示する文字列を決定し、表示
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 文字を表示するセルの取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // 表示したい文字の設定
+        cell.textLabel?.text = placeList[indexPath.row] as String
+        cell.accessoryType = .disclosureIndicator
+        
+        // 文字を設定さいたセルを返す
+        return cell
+        
+        
+    }
+    
+    
+    //セルがタップ（選択）されたとき発動
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("選択されたデータ:\(placeList[indexPath.row])")
+        
+        //選択されたエリア名をメンバ変数に格納
+        selectName = placeList[indexPath.row]
+        
+        //セグエを指定して画面移動
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    
+    // セグエを通って次の画面へ移動するとき
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // 移動先の画面のインスタンスを作成
+        var dvc = segue.destination as! second
+            
+        // 移動先の画面のプロパティにエリア名を指定
+        // dvc.myLalel.text = selectName 考え方はOKだが、インスタンス化されるタイミングがもっと後なのでエラーになってしまう
+        dvc.areaName = selectName
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ second.swift
@@ -0,0 +1,107 @@
+//
+//  second.swift
+//  sampleMiniGuidebook
+//
+//  Created by 那須真 on 2017/06/19.
+//  Copyright © 2017年 Makoto Nasu. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class second: UIViewController {
+
+    @IBOutlet weak var myTaxtView: UITextView!
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    var areaName = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let filePath = Bundle.main.path(forResource: "placelist", ofType: "plist")
+        
+        // プロパティリストからdataを取得（ディクショナリー型）
+        let dic = NSDictionary(contentsOfFile: filePath!)
+      
+        // 指定されたエリア名をキーにして、画面に必要な情報を取得
+        
+        let detailInfo:NSDictionary = dic![areaName] as! NSDictionary
+        
+        myTaxtView.text = (detailInfo["description"] as! String)
+        myImageView.image = UIImage(named:detailInfo["image"] as! String)
+        
+        print(detailInfo["image"] as! String)
+        print(detailInfo["description"] as! String)
+        
+        // 中心となる場所の座標オブジェクトを作成
+        
+        let coodinate = CLLocationCoordinate2DMake((Double(detailInfo["latitude"] as! String))!,(Double(detailInfo["longitude"] as! String)!))
+
+        
+        // 縮尺を設定
+        
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        
+        // 範囲オブジェクトを作成
+        
+        let region = MKCoordinateRegionMake(coodinate,span)
+        
+        // MapViewに周囲オブジェクトを設定
+        myMapView.setRegion(region, animated:true)
+        // アヤラにピンを立てる
+        // ピンを生成
+        
+        let myPin = MKPointAnnotation()
+        
+        // ピンの座標を設定
+        
+        myPin.coordinate = coodinate
+        //            myPin.coordinate = CLLocationCoordinate2DMake(10.317347,123.905759)
+        
+        // タイトル、サブタイトルを設定
+        
+        myPin.title = areaName
+        myPin.subtitle = (detailInfo["description"] as! String)
+
+
+        // mapViewにピンを追加
+        
+        myMapView.addAnnotation(myPin)
+    
+    }
+    
+    
+    // ツールバーのアクションボタンがおされたとき
+
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        
+        // アクティビティビューのインスタンスを作成
+        let controller = UIActivityViewController(activityItems:[myImageView.image!],applicationActivities: nil)
+        
+        // モーダル表示
+        self.present(controller,animated: true, completion: nil)
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
